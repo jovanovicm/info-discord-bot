146466428e66c66ef7940ae867a9a29d918b108d
jovanovicm <71355061+jovanovicm@users.noreply.github.com>
2021-02-27 17:26:54 -0500

    Create keywords.py

---
+++ keywords.py
@@ -0,0 +1,29 @@
+# ===== Projects ======
+
+# Discord Bot Project information
+discord_bot_keywords = ['discord', 'bot', 'project', 'february', 'projects', '2021', 'finished', 'done', 'completed',
+                        'all', 'worked', 'discord-bot', 0]
+
+# Discord Bot Project 2 information
+discord_bot2_keywords = ['discord', 'bot', 'project', '2', 'february', 'projects', '2021', 'progress', 'all', 'now',
+                         'current', 'currently', 0]
+
+# Computer Vision Project information
+cv_keywords = ['computer', 'cv', 'vision', 'project', 'march', 'projects', '2021', 'planned', 'all', 'soon', 'future',
+               'next', 0]
+
+# ====== Profiles ======
+
+# Marko Jovanovic
+marko_kw = ['exec', 'executive', 'founder', 'december', '2020', 'electrical', 'engineering', '3', 'discord', 'bot',
+            'marko', 'markos', 'jovanovic', 'guy', 'guys', 'male', 'males', 'research', 'researcher', 0]
+
+# Ishan Kumar
+ishan_kw = ['exec', 'executive', 'founder', 'december', '2020', 'electrical', 'engineering', '3', 'discord', 'bot',
+            'ishan', 'ishans', 'kumar', 'guy', 'guys', 'male', 'males', 0]
+
+# Iliana Meco
+iliana_kw = ['exec', 'executive', 'founder', 'december', '2020', 'electrical', 'engineering', '3', 'discord', 'bot',
+             'iliana', 'ilianas', 'meco', 'girl', 'girls', 'female', 'females', 0]
+
+kw_lists = [discord_bot_keywords, discord_bot2_keywords, cv_keywords, marko_kw, ishan_kw, iliana_kw]
