64330017c460682bf3cf3414ecb416211d40a27b
jovanovicm <71355061+jovanovicm@users.noreply.github.com>
2021-02-27 17:26:01 -0500

    Create embeds.py

---
+++ embeds.py
@@ -0,0 +1,53 @@
+import discord
+# ===== Projects =====
+
+# Discord Bot Project information
+embed1 = discord.Embed(title="Discord Bot Project", description='''An introductory project that gets everyone up to pace
+with python and to spice up the discord!  We wanted to add some automation to the server as well as some fun 
+through means of a discord bot.''', color=0x42acae)
+embed1.add_field(name="Starting Date", value="February 4th, 2021", inline=True)
+embed1.add_field(name="Progress", value="Completed", inline=True)
+embed1.set_footer(text="discord-bot")
+
+# Discord Bot Project 2 information
+embed2 = discord.Embed(title="Discord Bot Project 2", description='''An introductory project that gets everyone up to 
+pace with python and to spice up the discord!  We wanted to add some automation to the server as well as some fun games
+through means of a discord bot.''', color=0x42acae)
+embed2.add_field(name="Starting Date", value="February 4th, 2021", inline=True)
+embed2.add_field(name="Progress", value="In Progress", inline=True)
+embed2.set_footer(text="discord-bot-2")
+
+# Computer Vision Project information
+embed3 = discord.Embed(title="Computer Vision Project", description="Computer Vision Project", color=0x42acae)
+embed3.add_field(name="Starting Date", value="March 2021", inline=True)
+embed3.add_field(name="Progress", value="Planned", inline=True)
+embed3.set_footer(text="computer-vision")
+
+# ===== Profiles ======
+
+# Marko Jovanovic
+embed4 = discord.Embed(title="Description", description="i am a founder and exec for ai club :) I am a 3rd year electrical engineering student currently doing research using ai. it is super cool and thats why i wanna show you guys it")
+embed4.set_author(name="Marko Jovanovic", url="https://github.com/jovanovicm", icon_url="https://i.imgur.com/wSTFkRM.png")
+embed4.add_field(name="Discord", value="марко#7325", inline=False)
+embed4.add_field(name="Program", value="Electrical Engineering", inline=True)
+embed4.add_field(name="Year", value="3", inline=True)
+embed4.add_field(name="Joined Club", value="December 29th, 2020", inline=True)
+embed4.add_field(name="Project Experience", value="Discord Bot 🤖", inline=True)
+
+# Ishan Kumar
+embed5 = discord.Embed(title="Description", description="i am a founder and exec for ai club :) I am a 3rd year electrical engineering student. ")
+embed5.set_author(name="Ishan Kumar", url="https://github.com/jovanovicm", icon_url="https://i.imgur.com/wSTFkRM.png")
+embed5.add_field(name="Discord", value="IshisMAD#2484", inline=False)
+embed5.add_field(name="Program", value="Electrical Engineering", inline=True)
+embed5.add_field(name="Year", value="3", inline=True)
+embed5.add_field(name="Joined Club", value="December 29th, 2020", inline=True)
+embed5.add_field(name="Project Experience", value="Discord Bot 🤖", inline=True)
+
+# Iliana Meco
+embed6 = discord.Embed(title="Description", description="i am a founder and exec for ai club :) I am a 3rd year electrical engineering student. ")
+embed6.set_author(name="Iliana Meco", url="https://github.com/jovanovicm", icon_url="https://i.imgur.com/wSTFkRM.png")
+embed6.add_field(name="Discord", value="Iliana Meco#8364", inline=False)
+embed6.add_field(name="Program", value="Electrical Engineering", inline=True)
+embed6.add_field(name="Year", value="3", inline=True)
+embed6.add_field(name="Joined Club", value="December 29th, 2020", inline=True)
+embed6.add_field(name="Project Experience", value="Discord Bot 🤖", inline=True)
